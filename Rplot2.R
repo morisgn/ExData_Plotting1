ead40dd1ab99485ffb11ce0fa2e1de78006276a8
morisgn <morisgn@outlook.com>
2014-05-11 13:07:19 -0600

    Added ###  If you want to see the same label as mine on the x axis, you may want to set your LC_TIME to English.Initial submission of ExData_Plotting1

--- Rplot2.R
+++ Rplot2.R
@@ -2,7 +2,12 @@
 ### To execute this code, just type in your R console :: source("Rplot2.R")
 ###  household_power_consumption.txt should be in the same directory as the R code
 #####################################################################################
-
+###  If you want to see the same label as mine on the x axis, you may want to set your LC_TIME to English.
+### To set yout local time to another language, say French below is how you can do it.
+###     Sys.setlocale("LC_TIME", "fr")     # Solaris: details are OS-dependent
+###     Sys.setlocale("LC_TIME", "fr_FR.utf8")   #  Linux 
+###     Sys.setlocale("LC_TIME", "fr_FR")  # Mac OS X, in UTF-8
+###     Sys.setlocale("LC_TIME", "French") # Windows
 
 # Read data and truck values from household_power_consumption.txt. Missing values (?) are replaced with NA . 
 rawData<- read.table("household_power_consumption.txt", header= TRUE, sep=";", na.strings = c("?"))
